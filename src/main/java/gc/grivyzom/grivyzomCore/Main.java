6311414c4d23fbad23daf626285abfd8f0a264a2
Brocolitx <grivyzom@gmail.com>
2025-06-24 22:14:10 -0400

    first commit

---
+++ src/main/java/gc/grivyzom/grivyzomCore/Main.java
@@ -0,0 +1,18 @@
+package gc.grivyzom.grivyzomCore;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(id = "grivyzomcore", name = "GrivyzomCore", version = "0.1-SNAPSHOT", url = "www.grivyzom.com", authors = {"Francisco Fuentes"})
+public class Main {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+}
